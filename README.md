9620bb57675ce5683e5665bb525b14874b34f890
Hung Bui <hung.bui@weploy.com.vn>
2023-11-05 19:20:10 +0700

    [quotes][init commit]

---
+++ README.md
@@ -0,0 +1,21 @@
+# Installation
+REQUIRES: npm need to be install first.
+https://docs.npmjs.com/downloading-and-installing-node-js-and-npm
+## Development ENV
+
+At the project folder path run these commands:
+
+```
+npm install
+npm run build
+npm start
+```
+
+![alt text](https://github.com/HungBNDev/quotes/blob/main/preview.png)
+
+## Functional Ingredients
+
+[Techs](https://app.diagrams.net/#G1eE1h3DBlFAIYwDgdkumcoWWJGU-rO5ms)
+## See the website in this link
+
+[Website](https://react-709e9.web.app/quotes)
---
+++ src/App.js
@@ -0,0 +1,49 @@
+import React, { Suspense } from 'react';
+import { Route, Switch, Redirect } from 'react-router-dom';
+
+
+import Layout from './components/layout/Layout';
+
+
+import LoadingSpinner from './components/UI/LoadingSpinner';
+
+const NewQuote = React.lazy(() => import('./pages/NewQuote'));
+const QuoteDetail = React.lazy(() => import('./pages/QuoteDetail'));
+const NotFound = React.lazy(() => import('./pages/NotFound'));
+const AllQuotes = React.lazy(() => import('./pages/AllQuotes'));
+
+function App() {
+  return (
+    <Layout>
+      <Suspense
+        fallback={
+          <div className='centered'>
+            <LoadingSpinner />
+          </div>}
+      >
+        <Switch>
+          <Route path='/' exact>
+            <Redirect to='/quotes' />
+          </Route>
+          <Route path='/quotes' exact>
+            <AllQuotes></AllQuotes>
+          </Route>
+
+          <Route path='/quotes/:quoteId'>
+            <QuoteDetail></QuoteDetail>
+          </Route>
+
+          <Route path='/new-quote'>
+            <NewQuote></NewQuote>
+          </Route>
+
+          <Route path='*'>
+            <NotFound></NotFound>
+          </Route>
+        </Switch>
+      </Suspense>
+    </Layout>
+  );
+}
+
+export default App;
